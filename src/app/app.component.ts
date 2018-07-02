289ac5479fa45f0cfdba4e389232ed9094fd8acb
david remy <david.remy94@gmail.com>
2018-07-02 12:14:05 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+    var config = {
+      apiKey: "AIzaSyD7PtuJf_yrMkMfUYsueQ-TA_sIcIKc5gE",
+      authDomain: "bibliotheque-api.firebaseapp.com",
+      databaseURL: "https://bibliotheque-api.firebaseio.com",
+      projectId: "bibliotheque-api",
+      storageBucket: "gs://bibliotheque-api.appspot.com",
+      messagingSenderId: "658388583917"
+    };
+    firebase.initializeApp(config);
+  }
 }
---
+++ src/app/auth/signin/signin.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  signinForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authServise: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit(){
+    const email = this.signinForm.get('email').value;
+    const password = this.signinForm.get('password').value;
+
+    this.authServise.signInUser(email,password).then(
+      () => {
+        this.router.navigate(['/books']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
+
+}
